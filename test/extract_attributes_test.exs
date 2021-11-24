e8b26446c054250395043352df17914ceceeee93
Eduard Litau <eduard.litau@gmail.com>
2021-11-24 23:27:06 +0100

    mix format

--- test/extract_attributes_test.exs
+++ test/extract_attributes_test.exs
@@ -4,7 +4,8 @@ defmodule SpotTheStation.EventAttributesTest do
   @description "\n\t\t\t\tDate: Friday Dec 21, 2016 <br/>\n\t\t\t\tTime: 10:59 PM <br/>\n\t\t\t\tDuration: less than 3 minute <br/>\n\t\t\t\tMaximum Elevation: 81° <br/>\n\t\t\t\tApproach: 10° above W <br/>\n\t\t\t\tDeparture: 11° above E <br/>\n\t\t\t\t"
 
   test "extract start_time" do
-    assert {{2016, 12, 21}, {22, 59, 0}} == EventAttributes.extract(@description)[:start_time]
+    assert {{2016, 12, 21}, {22, 59, 0}} ==
+             EventAttributes.extract(@description)[:start_time]
   end
 
   test "extract end" do
@@ -12,6 +13,7 @@ defmodule SpotTheStation.EventAttributesTest do
   end
 
   test "summary" do
-    assert "ISS is above you for 3 minutes appearing 10° above W and disappearing 11° above E." == EventAttributes.extract(@description)[:summary]
+    assert "ISS is above you for 3 minutes appearing 10° above W and disappearing 11° above E." ==
+             EventAttributes.extract(@description)[:summary]
   end
 end
